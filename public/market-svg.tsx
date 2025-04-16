0c2fda7210b5f04ae9f3938d8621cfbc36af682d
Itai Biton <itaibiton@Itais-MacBook-Pro.local>
2025-04-16 15:10:37 +0300

    initial commit

---
+++ public/market-svg.tsx
@@ -0,0 +1,19 @@
+const MarkerSvg = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <foreignObject x="-9" y="-11" width="42" height="46"><div></div></foreignObject><g data-figma-bg-blur-radius="12">
+                <path d="M12 22C13 17 20 16.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 16.4183 11 17 12 22Z" fill="white" fillOpacity="0.05" />
+                <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" fill="white" fillOpacity="0.05" />
+                <path d="M12 22C13 17 20 16.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 16.4183 11 17 12 22Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="bgblur_0_60_282_clip_path" transform="translate(9 11)"><path d="M12 22C13 17 20 16.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 16.4183 11 17 12 22Z" />
+                    <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" />
+                </clipPath></defs>
+        </svg>
+
+    )
+}
+
+export default MarkerSvg;
