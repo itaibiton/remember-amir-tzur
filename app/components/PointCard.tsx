25bead3a2500c49ebf8da8bcae585b7b8994cbf2
Itai Biton <itaibiton@Itais-MacBook-Pro.local>
2025-05-06 21:50:41 +0300

    fix

--- app/components/PointCard.tsx
+++ app/components/PointCard.tsx
@@ -15,7 +15,8 @@ export interface Point {
 const PointCard = ({ point }: { point: Point }) => {
     return (
         <Link
-            href={point?.disabled ? '#' : point?.link} className={clsx(`flex border border-[#E0DFDC] bg-[#F7F6F2] rounded-[24px] py-4 px-6 gap-2 text-base md:text-lg`, {
+            target={point?.disabled ? '_blank' : '_self'}
+            href={point?.disabled ? '/' : point?.link} className={clsx(`flex border border-[#E0DFDC] bg-[#F7F6F2] rounded-[24px] py-4 px-6 gap-2 text-base md:text-lg`, {
                 'opacity-50': point?.disabled
             })}>
             <MarkerSvg />
