bb166830e50591ed1d66c218dc9fbdd17a39e8e5
Itai Biton <itaibiton@Itais-MacBook-Pro.local>
2025-05-07 13:19:27 +0300

    fix minor text

--- app/components/PointsSection.tsx
+++ app/components/PointsSection.tsx
@@ -6,7 +6,7 @@ const PointsSection = () => {
     return (
         <>
             <p className="text-[#3c3c3c] text-2xl font-semibold">הנקודות בפרויקט</p>
-            <p className="text-[#3c3c3c] text-base font-normal mb-6">בחרו נקודה כדי לדעת מה החיבור של אמיר אליה.</p>
+            <p className="text-[#3c3c3c] text-base font-normal mb-6">בחרו את הנקודה בה אתם נמצאים</p>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mb-15 w-full">
                 {POINTS.map((point, index) => (
                     <PointCard key={`${index}-point`} point={point} />
