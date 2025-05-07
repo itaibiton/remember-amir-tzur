c4d1d5d3546f04455f90149dbd08e7c53691138f
Itai Biton <itaibiton@Itais-MacBook-Pro.local>
2025-05-07 13:10:53 +0300

    fix amir image

--- app/components/ContentSection.tsx
+++ app/components/ContentSection.tsx
@@ -14,8 +14,10 @@ const ContentSection = () => {
                 ))}
                 {/* <p>יהי זכרו נצור בליבנו לעד.</p> */}
             </div>
-            <div className="flex flex-col h-fit w-full md:w-1/2 items-center">
-                <Image alt="amir-logo" src="/Amir-hero-image.png" width="552" height="575" />
+            <div className="flex flex-col h-fit w-full md:w-1/2 items-center  justify-center relative">
+                {/* <Image alt="amir-logo" src="/Amir-hero-image.png" width="552" height="575" /> */}
+                <div className="absolute w-[552px] h-[575px] rounded-full blur-3xl opacity-20  md:opacity-30 z-0" style={{ background: '#FFE9A7' }} />
+                <Image className='relative z-10' alt="amir-logo" src="/Amir-hero-clean.png" width="552" height="575" />
             </div>
         </div>
     );
