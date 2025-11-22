1dc08e9710ad26b026339f5dda488cb81c146d57
sikilouwich <amasa.louwich@mail.huji.ac.il>
2025-11-22 18:33:53 +0200

    Footer: take right.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="flex justify-center w-full">
+        <div className="flex justify-right w-full">
             <p className="text-[5d5d5d] text-xs pb-10">&apos;שלא נצא בורים&apos; - פרויקט ההנצחה ברוחו של אמיר צור</p>
         </div>
     );
